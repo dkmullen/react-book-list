4112adae6d433cfd0749b22468e513b58611cf23
Dennis Mullen <dkmullen@gmail.com>
2018-03-01 19:28:49 -0500

    first commit

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function () {
+  return [
+    {title: 'JavaScript: The Good Parts'},
+    {title: 'Shoeless Joe'},
+    {title: 'The Great Gatsby'},
+    {title: 'The Red Badge of Courage'}
+
+  ]
+}
